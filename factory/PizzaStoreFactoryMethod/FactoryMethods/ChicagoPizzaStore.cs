1c47950b49def70a59f762496415eddbd057aafe
Dani Giurizatto Utsch <daniutsch@outlook.com>
2022-04-15 23:00:18 +0200

    Adds `PizzaStoreFactoryMethod` project to `Factory` Design Pattern.

---
+++ factory/PizzaStoreFactoryMethod/FactoryMethods/ChicagoPizzaStore.cs
@@ -0,0 +1,19 @@
+﻿using PizzaStoreFactoryMethod.Products.Pizzas;
+
+namespace PizzaStoreFactoryMethod.FactoryMethods
+{
+    public class ChicagoPizzaStore : PizzaStore
+    {
+        protected override Pizza? CreatePizza(string item)
+        {
+            return item switch
+            {
+                "cheese" => new ChicagoStyleCheesePizza(),
+                "veggie" => new ChicagoStyleVeggiePizza(),
+                "clam" => new ChicagoStyleClamPizza(),
+                "pepperoni" => new ChicagoStylePepperoniPizza(),
+                _ => null,
+            };
+        }
+    }
+}
---
+++ factory/PizzaStoreFactoryMethod/FactoryMethods/NYPizzaStore.cs
@@ -0,0 +1,19 @@
+﻿using PizzaStoreFactoryMethod.Products.Pizzas;
+
+namespace PizzaStoreFactoryMethod.FactoryMethods
+{
+    public class NYPizzaStore : PizzaStore
+    {
+        protected override Pizza? CreatePizza(string item)
+        {
+            return item switch
+            {
+                "cheese" => new NYStyleCheesePizza(),
+                "veggie" => new NYStyleVeggiePizza(),
+                "clam" => new NYStyleClamPizza(),
+                "pepperoni" => new NYStylePepperoniPizza(),
+                _ => null,
+            };
+        }
+    }
+}
