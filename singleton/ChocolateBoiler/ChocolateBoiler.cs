e74f075a238a6e1819b501c9b6576439c47a3f1a
Dani Giurizatto Utsch <daniutsch@outlook.com>
2022-04-16 14:47:52 +0200

    Adds `ChocolateBoiler` project to `Singleton` Design Pattern.

---
+++ singleton/ChocolateBoiler/ChocolateBoiler.cs
@@ -0,0 +1,55 @@
+﻿namespace ChocolateBoiler
+{
+    public sealed class ChocolateBoiler
+    {
+        public bool Empty { get; private set; }
+        public bool Boiled { get; private set; }
+
+        private static ChocolateBoiler? _uniqueInstace;
+
+        private ChocolateBoiler()
+        {
+            Empty = true;
+            Boiled = false;
+        }
+
+        public static ChocolateBoiler Instance
+        {
+            get
+            {
+                if (_uniqueInstace == null)
+                {
+                    Console.WriteLine("Creating unique instance of Chocolate Boiler");
+                    _uniqueInstace = new ChocolateBoiler();
+                }
+                Console.WriteLine("Returning instance of Chocolate Boiler");
+                return _uniqueInstace;
+            }
+        }
+
+        public void Fill()
+        {
+            if (Empty)
+            {
+                Empty = false;
+                Boiled = false;
+            }
+        }
+
+        public void Drain()
+        {
+            if (!Empty && Boiled)
+            {
+                Empty = true;
+            }
+        }
+
+        public void Boil()
+        {
+            if (!Empty && !Boiled)
+            {
+                Boiled = true;
+            }
+        }
+    }
+}
---
+++ singleton/ChocolateBoiler/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace ChocolateBoiler
+{
+    public static class Program
+    {
+        public static void Main()
+        {
+            ChocolateBoiler boiler = ChocolateBoiler.Instance;
+            boiler.Fill();
+            boiler.Boil();
+            boiler.Drain();
+
+            // will return the existing instance
+#pragma warning disable IDE0059 // Unnecessary assignment of a value
+            ChocolateBoiler boiler2 = ChocolateBoiler.Instance;
+#pragma warning restore IDE0059 // Unnecessary assignment of a value
+        }
+    }
+}
