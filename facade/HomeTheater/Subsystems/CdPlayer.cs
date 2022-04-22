fdb9786fa5b22b389cb456d52a7d5683d62a094d
Dani Giurizatto Utsch <daniutsch@outlook.com>
2022-04-22 23:03:15 +0200

    Adds `HomeTheater` project to `Façades` Design Pattern.

---
+++ facade/HomeTheater/Subsystems/CdPlayer.cs
@@ -0,0 +1,71 @@
+﻿namespace HomeTheater
+{
+    public class CdPlayer
+    {
+        private readonly string _description;
+#pragma warning disable IDE0052 // Remove unread private members
+        private readonly Amplifier _amplifier;
+#pragma warning restore IDE0052 // Remove unread private members
+        private string? _title;
+        private int _currentTrack;
+
+        public CdPlayer(string description, Amplifier amplifier)
+        {
+            _description = description;
+            _amplifier = amplifier;
+        }
+
+        public void On()
+        {
+            Console.WriteLine($"{_description} on");
+        }
+
+        public void Off()
+        {
+            Console.WriteLine($"{_description} off");
+        }
+
+        public void Eject()
+        {
+            _title = null;
+            Console.WriteLine($"{_description} eject");
+        }
+
+        public void Play(string title)
+        {
+            _title = title;
+            _currentTrack = 0;
+
+            Console.WriteLine($"{_description} playing \"{_title}\"");
+        }
+
+        public void Play(int track)
+        {
+            if (_title == null)
+            {
+                Console.WriteLine($"{_description} can't play track {_currentTrack}, no cd inserted");
+            }
+            else
+            {
+                _currentTrack = track;
+                Console.WriteLine($"{_description} playing track {_currentTrack}");
+            }
+        }
+
+        public void Stop()
+        {
+            _currentTrack = 0;
+            Console.WriteLine($"{_description} stopped");
+        }
+
+        public void Pause()
+        {
+            Console.WriteLine($"{_description} paused \"{_title}\"");
+        }
+
+        public override string ToString()
+        {
+            return _description;
+        }
+    }
+}
---
+++ facade/HomeTheater/Subsystems/Tuner.cs
@@ -0,0 +1,50 @@
+﻿using System.Globalization;
+
+namespace HomeTheater
+{
+    public class Tuner
+    {
+        private readonly string _description;
+#pragma warning disable IDE0052 // Remove unread private members
+        private readonly Amplifier _amplifier;
+        private double _frequency;
+#pragma warning restore IDE0052 // Remove unread private members
+
+        public Tuner(string description, Amplifier amplifier)
+        {
+            _description = description;
+            _amplifier = amplifier;
+        }
+
+        public void On()
+        {
+            Console.WriteLine($"{_description} on");
+        }
+
+        public void Off()
+        {
+            Console.WriteLine($"{_description} off");
+        }
+
+        public void SetFrequency(double frequency)
+        {
+            Console.WriteLine($"{_description} setting frequency to {frequency.ToString(CultureInfo.InvariantCulture)}");
+            _frequency = frequency;
+        }
+
+        public void SetAm()
+        {
+            Console.WriteLine($"{_description} setting AM mode");
+        }
+
+        public void SetFm()
+        {
+            Console.WriteLine($"{_description} setting FM mode");
+        }
+
+        public override string ToString()
+        {
+            return _description;
+        }
+    }
+}
