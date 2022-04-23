8a601e04ec15138bda58f49daeebd0156c87a68c
Dani Giurizatto Utsch <daniutsch@outlook.com>
2022-04-23 11:15:55 +0200

    Adds `Barista` project to `Template Method` Design Pattern.

---
+++ template_method/Barista/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace Barista
+{
+    public static class Program
+    {
+        public static void Main()
+        {
+            Tea tea = new();
+            Coffee coffee = new();
+
+            Console.WriteLine("Making tea...");
+            tea.PrepareRecipe();
+
+            Console.WriteLine("\nMaking coffee...");
+            coffee.PrepareRecipe();
+
+            TeaWithHook teaHook = new();
+            CoffeeWithHook coffeeHook = new();
+
+            Console.WriteLine("\nMaking tea...");
+            teaHook.PrepareRecipe();
+
+            Console.WriteLine("\nMaking coffee...");
+            coffeeHook.PrepareRecipe();
+        }
+    }
+}
---
+++ template_method/Barista/TeaWithHook.cs
@@ -0,0 +1,26 @@
+﻿namespace Barista
+{
+    public class TeaWithHook : CaffeineBeverageWithHook
+    {
+        public override void Brew()
+        {
+            Console.WriteLine("Steeping the tea");
+        }
+
+        public override void AddCondiments()
+        {
+            Console.WriteLine("Adding Lemon");
+        }
+
+        public override bool CustomerWantsCondiments => GetUserInput().StartsWith("Y", StringComparison.OrdinalIgnoreCase);
+
+        private static string GetUserInput()
+        {
+            Console.WriteLine("Would you like lemon with your tea (y/n)? ");
+
+            string? answer = Console.ReadLine();
+
+            return answer ?? "no";
+        }
+    }
+}
