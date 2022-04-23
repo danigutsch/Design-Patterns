56925a4dea5b092da4a94c253d687b0b90abc372
Dani Giurizatto Utsch <daniutsch@outlook.com>
2022-04-23 11:41:05 +0200

    Adds `DuckSort` project to `Template Method` Design Pattern.

---
+++ template_method/DuckSort/Duck.cs
@@ -0,0 +1,25 @@
+﻿namespace DuckSort
+{
+    public class Duck : IComparable<Duck>
+    {
+        public string Name { get; }
+        public int Weight { get; }
+
+        public Duck(string name, int weight)
+        {
+            Name = name;
+            Weight = weight;
+        }
+
+        public override string ToString()
+        {
+            return Name + " weighs " + Weight;
+        }
+
+        public int CompareTo(Duck? otherDuck)
+        {
+            if (otherDuck == null) return 1;
+            return Weight.CompareTo(otherDuck.Weight);
+        }
+    }
+}
---
+++ template_method/DuckSort/Program.cs
@@ -0,0 +1,34 @@
+﻿namespace DuckSort
+{
+    public static class Program
+    {
+        public static void Main()
+        {
+            Duck[] ducks =
+            {
+                new Duck("Daffy", 8),
+                new Duck("Dewey", 2),
+                new Duck("Howard", 7),
+                new Duck("Louie", 2),
+                new Duck("Donald", 10),
+                new Duck("Huey", 2)
+            };
+
+            Console.WriteLine("Before sorting:");
+            Display(ducks);
+
+            Array.Sort(ducks);
+
+            Console.WriteLine("\nAfter sorting:");
+            Display(ducks);
+        }
+
+        public static void Display(IEnumerable<Duck> ducks)
+        {
+            foreach (Duck duck in ducks)
+            {
+                Console.WriteLine(duck);
+            }
+        }
+    }
+}
