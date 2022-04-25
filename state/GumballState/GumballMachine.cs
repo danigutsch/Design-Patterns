74cbfa22c54ae841de2d4eee7cbb4af97ede3905
Dani Giurizatto Utsch <daniutsch@outlook.com>
2022-04-25 11:44:10 +0200

    Adds `GumballState` project to `State` Design Pattern.

---
+++ state/GumballState/GumballMachine.cs
@@ -0,0 +1,81 @@
+﻿using GumballState.States;
+using System.Text;
+
+namespace GumballState
+{
+    public class GumballMachine
+    {
+        public IState SoldOutState { get; }
+        public IState HasQuarterState { get; }
+        public IState NoQuarterState { get; }
+        public IState SoldState { get; }
+
+        public int Count { get; private set; }
+        public IState State { get; private set; }
+
+        public GumballMachine(int numberGumballs)
+        {
+            SoldOutState = new SoldOutState(this);
+            HasQuarterState = new HasQuarterState(this);
+            NoQuarterState = new NoQuarterState(this);
+            SoldState = new SoldState(this);
+
+            Count = numberGumballs;
+            State = numberGumballs > 0 ? NoQuarterState : SoldOutState;
+        }
+
+        public void InsertQuarter()
+        {
+            State.InsertQuarter();
+        }
+
+        public void EjectQuarter()
+        {
+            State.EjectQuarter();
+        }
+
+        public void TurnCrank()
+        {
+            State.TurnCrank();
+            State.Dispense();
+        }
+
+        public void SetState(IState state)
+        {
+            State = state;
+        }
+
+        public void ReleaseBall()
+        {
+            Console.WriteLine("A gumball comes rolling out the slot...");
+            if (Count > 0)
+            {
+                Count--;
+            }
+        }
+
+        public void Refill(int count)
+        {
+            Count += count;
+            Console.WriteLine($"The gumball machine was just refilled; it's new count is: {Count}");
+            State.Refill();
+        }
+
+        public override string ToString()
+        {
+            StringBuilder result = new();
+
+            result.Append("\nMighty Gumball, Inc.");
+            result.Append("\nC#-enabled Standing Gumball Model #2016");
+            result.Append("\nInventory: ").Append(Count).Append(" gumball");
+            if (Count != 1)
+            {
+                result.Append('s');
+            }
+            result.Append('\n');
+            result.Append("Machine is ").Append(State).Append('\n');
+
+            return result.ToString();
+        }
+    }
+}
---
+++ state/GumballState/States/HasQuarterState.cs
@@ -0,0 +1,38 @@
+﻿namespace GumballState.States
+{
+    public class HasQuarterState : IState
+    {
+        private readonly GumballMachine _gumballMachine;
+
+        public HasQuarterState(GumballMachine gumballMachine)
+        {
+            _gumballMachine = gumballMachine;
+        }
+
+        public void InsertQuarter()
+        {
+            Console.WriteLine("You can't insert another quarter");
+        }
+
+        public void EjectQuarter()
+        {
+            Console.WriteLine("Quarter returned");
+            _gumballMachine.SetState(_gumballMachine.NoQuarterState);
+        }
+
+        public void TurnCrank()
+        {
+            Console.WriteLine("You turned...");
+            _gumballMachine.SetState(_gumballMachine.SoldState);
+        }
+
+        public void Dispense()
+        {
+            Console.WriteLine("No gumball dispensed");
+        }
+
+        public void Refill() { }
+
+        public override string ToString() => "waiting for turn of crank";
+    }
+}
