31c985e86b206a6de99dbd07a6cf952397eeeb9e
Dani Giurizatto Utsch <daniutsch@outlook.com>
2022-04-25 09:34:18 +0200

    Adds `Menu` project to `Composite` Design Pattern.

---
+++ composite/Menu/Menu.cs
@@ -0,0 +1,40 @@
+﻿namespace Menu
+{
+    public class Menu : MenuComponent
+    {
+        private readonly List<MenuComponent> _menuComponents = new();
+
+        public Menu(string name, string description)
+        {
+            Name = name;
+            Description = description;
+        }
+
+        public override string Name { get; }
+        public override string Description { get; }
+
+        public override void Add(MenuComponent menuComponent)
+        {
+            _menuComponents.Add(menuComponent);
+        }
+
+        public override void Remove(MenuComponent menuComponent)
+        {
+            _menuComponents.Remove(menuComponent);
+        }
+
+        public override MenuComponent GetChild(int i) => _menuComponents[i];
+
+        public override void Print()
+        {
+            Console.Write("\n" + Name);
+            Console.WriteLine(", " + Description);
+            Console.WriteLine("---------------------");
+
+            foreach (var menuComponent in _menuComponents)
+            {
+                menuComponent.Print();
+            }
+        }
+    }
+}
---
+++ composite/Menu/MenuItem.cs
@@ -0,0 +1,31 @@
+﻿using System.Globalization;
+
+namespace Menu
+{
+    public class MenuItem : MenuComponent
+    {
+        public MenuItem(string name, string description, bool isVegetarian, decimal price)
+        {
+            Name = name;
+            Description = description;
+            Price = price;
+            IsVegetarian = isVegetarian;
+        }
+
+        public override string Name { get; }
+        public override string Description { get; }
+        public override decimal Price { get; }
+        public override bool IsVegetarian { get; }
+
+        public override void Print()
+        {
+            Console.Write(" " + Name);
+            if (IsVegetarian)
+            {
+                Console.Write("(v)");
+            }
+            Console.WriteLine(", " + Price.ToString(CultureInfo.InvariantCulture));
+            Console.WriteLine("    --" + Description);
+        }
+    }
+}
